52221f8c1a314f36f2ed1959227aea885f307fd7
Adetunji Adebowale <66047485+KoolTeck@users.noreply.github.com>
2020-09-08 17:55:26 +0100

    adding database

--- server.js
+++ server.js
@@ -10,10 +10,8 @@ const profile = require('./controllers/profile');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'kooljoe247',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
